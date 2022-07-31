558c7189ae32cdf2aa3a4d0dca835bf2a39379f2
Miadern <465029437@qq.com>
2022-07-31 10:08:43 +0800

    完成了项目初始配置

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,8 @@
 import request from '@/utils/request'
 
-export function login(data) {
+export function login() {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: '',
     method: 'post',
-    data
-  })
-}
-
-export function getInfo(token) {
-  return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
-
-export function logout() {
-  return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
   })
 }
